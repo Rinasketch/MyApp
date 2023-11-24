c329aae015f118b98d7e5954bb168d2fffd354d4
Rinasketch <148871412+Rinasketch@users.noreply.github.com>
2023-11-24 20:09:38 +0900

    Add files via upload

---
+++ MyFirstProject1/MyFirstProject1/MyFirstProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProject1App.swift
+//  MyFirstProject1
+//
+//  Created by Nari Kim on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
